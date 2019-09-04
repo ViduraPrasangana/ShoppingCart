697a3c9d6260d33c9ad104e39dd59c6994df2034
Achinths Isuru <achinthaisuru444@gmail.com>
2019-09-04 18:56:09 +0530

    Partially finished the AccountFragment
    But we have to connect the database , after that we can simply change this class, and we may have to change the Account Fragment Interface design as well.

--- app/src/main/java/com/ayesha/shoppingcart/AccountFragment.java
+++ app/src/main/java/com/ayesha/shoppingcart/AccountFragment.java
@@ -12,7 +12,6 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textview.MaterialTextView;
 
 public class AccountFragment extends Fragment {
 
@@ -30,8 +29,8 @@ public class AccountFragment extends Fragment {
     private TextInputEditText numberEdit;
     private TextInputEditText addrEdit;
 
-    private MaterialButton btnLogin ;
-    private MaterialButton btnRegister;
+    private MaterialButton btnEdit ;
+    private MaterialButton btnSave;
 
     @Nullable
     @Override
@@ -47,16 +46,77 @@ public class AccountFragment extends Fragment {
         addr = view.findViewById(R.id.addr);
 
         firstNameEdit = view.findViewById(R.id.firstNameEdit);
-        lastNameEdit = view.findViewById(R.id.firstNameEdit);
-        emailEdit = view.findViewById(R.id.firstNameEdit);
-        numberEdit = view.findViewById(R.id.firstNameEdit);
-        addrEdit = view.findViewById(R.id.firstNameEdit);
+        lastNameEdit = view.findViewById(R.id.lastNameEdit);
+        emailEdit = view.findViewById(R.id.emailEdit);
+        numberEdit = view.findViewById(R.id.numberEdit);
+        addrEdit = view.findViewById(R.id.addrEdit);
 
+        btnEdit = view.findViewById(R.id.btnEdit);
+        btnSave = view.findViewById(R.id.btnSave);
+        //------------------------------//
 
-
+        this.btnEditListner(); //btnEdit onClickListner
+        this.btnSaveListner(); //btnSave onClickListner
 
         return view;
 
     }
-    
+
+    private void btnEditListner(){
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Change the visibility of the TextView to INVISIBLE
+                firstName.setVisibility(View.INVISIBLE);
+                lastName.setVisibility(View.INVISIBLE);
+                email.setVisibility(View.INVISIBLE);
+                number.setVisibility(View.INVISIBLE);
+                addr.setVisibility(View.INVISIBLE);
+
+                //Change the visibility of the EditText to VISIBLE
+                firstNameEdit.setVisibility(View.VISIBLE);
+                lastNameEdit.setVisibility(View.VISIBLE);
+                emailEdit.setVisibility(View.VISIBLE);
+                numberEdit.setVisibility(View.VISIBLE);
+                addrEdit.setVisibility(View.VISIBLE);
+
+                //Change the btnSave to enable
+                btnSave.setEnabled(true);
+
+                //Change the btnEdit to enable
+                btnEdit.setEnabled(false);
+            }
+        });
+    }
+
+    private void btnSaveListner(){
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Change the btnEdit to enable
+                btnEdit.setEnabled(true);
+
+                //Change the btnSave to disable
+                btnSave.setEnabled(false);
+
+                //Change the visibility of the TextView to VISIBLE
+                firstName.setVisibility(View.VISIBLE);
+                lastName.setVisibility(View.VISIBLE);
+                email.setVisibility(View.VISIBLE);
+                number.setVisibility(View.VISIBLE);
+                addr.setVisibility(View.VISIBLE);
+
+                //Change the visibility of the EditText to INVISIBLE
+                firstNameEdit.setVisibility(View.INVISIBLE);
+                lastNameEdit.setVisibility(View.INVISIBLE);
+                emailEdit.setVisibility(View.INVISIBLE);
+                numberEdit.setVisibility(View.INVISIBLE);
+                addrEdit.setVisibility(View.INVISIBLE);
+
+                //This is a simple Toast Message
+                Constants.showToast(getActivity(),"SAVED");
+            }
+        });
+    }
 }
