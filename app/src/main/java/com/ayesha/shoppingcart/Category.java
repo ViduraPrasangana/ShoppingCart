0389e21a75b1436c7f9763a13fc984aa5b02bd12
Achinths Isuru <achinthaisuru444@gmail.com>
2019-09-04 19:20:45 +0530

    Updated the Category Class

--- app/src/main/java/com/ayesha/shoppingcart/Category.java
+++ app/src/main/java/com/ayesha/shoppingcart/Category.java
@@ -11,4 +11,37 @@ package com.ayesha.shoppingcart;
 //--Chilled
 
 public class Category {
+
+    private int id, image;
+    private String name;
+
+    public Category(int id, int image, String name) {
+        this.id = id;
+        this.image = image;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
