6d5c833fdb90fe7fa59164cd5d0a281786a38bde
Achinths Isuru <achinthaisuru444@gmail.com>
2019-09-04 18:30:07 +0530

    Finished the account interface
    Started in the AccountFragment class and changed the User class

--- app/src/main/java/com/ayesha/shoppingcart/AccountFragment.java
+++ app/src/main/java/com/ayesha/shoppingcart/AccountFragment.java
@@ -4,16 +4,59 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textview.MaterialTextView;
+
 public class AccountFragment extends Fragment {
+
+    private User user; //User object that will show
+
+    private TextView firstName;
+    private TextView lastName;
+    private TextView email;
+    private TextView number;
+    private TextView addr;
+
+    private TextInputEditText firstNameEdit;
+    private TextInputEditText lastNameEdit;
+    private TextInputEditText emailEdit;
+    private TextInputEditText numberEdit;
+    private TextInputEditText addrEdit;
+
+    private MaterialButton btnLogin ;
+    private MaterialButton btnRegister;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.account_fragment,container,false);
+
+        View view = inflater.inflate(R.layout.account_fragment,container,false);
+
+        //Finding components from the view
+        firstName = view.findViewById(R.id.firstName);
+        lastName = view.findViewById(R.id.lastName);
+        email = view.findViewById(R.id.email);
+        number = view.findViewById(R.id.number);
+        addr = view.findViewById(R.id.addr);
+
+        firstNameEdit = view.findViewById(R.id.firstNameEdit);
+        lastNameEdit = view.findViewById(R.id.firstNameEdit);
+        emailEdit = view.findViewById(R.id.firstNameEdit);
+        numberEdit = view.findViewById(R.id.firstNameEdit);
+        addrEdit = view.findViewById(R.id.firstNameEdit);
+
+
+
+
+        return view;
 
     }
+    
 }
--- app/src/main/java/com/ayesha/shoppingcart/User.java
+++ app/src/main/java/com/ayesha/shoppingcart/User.java
@@ -1,19 +1,57 @@
 package com.ayesha.shoppingcart;
+import android.os.Parcelable;
+
 import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
-public class User {
-     public String email, firstName, lastName, number, address;
+public class User  {
+     private String email, firstName, lastName, number, address;
 
-     public User(){
-
-     }
-
-     User(String email, String firstName, String lastName, String number, String address) {
+     public User(String email, String firstName, String lastName, String number, String address) {
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.number = number;
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
     }
 }
