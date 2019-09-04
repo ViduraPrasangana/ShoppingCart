1ed6412f0d7b07b57a06df477c4b1f0f8f3845d6
HunterAlex <vidura.prasangana16@gmail.com>
2019-09-04 12:04:26 +0530

    product class added

--- app/src/main/java/com/ayesha/shoppingcart/Product.java
+++ app/src/main/java/com/ayesha/shoppingcart/Product.java
@@ -1,7 +1,13 @@
 package com.ayesha.shoppingcart;
 
 public class Product {
-    private String name;
+    private String name, description, imgUrl;
     private double price;
 
+    public Product(String name, String description, String imgUrl, double price) {
+        this.name = name;
+        this.description = description;
+        this.imgUrl = imgUrl;
+        this.price = price;
+    }
 }
