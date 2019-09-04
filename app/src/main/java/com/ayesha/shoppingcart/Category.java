15c4b0659655393498fe97bd781d007dd29bc6a0
Achinths Isuru <achinthaisuru444@gmail.com>
2019-09-04 19:17:51 +0530

    Created Cateogry Class

---
+++ app/src/main/java/com/ayesha/shoppingcart/Category.java
@@ -0,0 +1,14 @@
+package com.ayesha.shoppingcart;
+
+//Categories!//
+//--Frozen food
+//--Grocery
+//--Vegetables
+//--Fruits
+//--Meat
+//--Fish
+//--Liquor
+//--Chilled
+
+public class Category {
+}
