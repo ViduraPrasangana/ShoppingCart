2f5fa4dc2f0df781a9dae796a38ceb38c7ef1818
vidura prasangana <vidura.prasangana16@gmail.com>
2019-09-11 23:10:30 +0530

    little bug

--- app/src/main/java/com/ayesha/shoppingcart/CartFragment.java
+++ app/src/main/java/com/ayesha/shoppingcart/CartFragment.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +13,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
