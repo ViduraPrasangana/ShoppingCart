40372664b734e9a753a5c45f6593975258a29802
Achinths Isuru <achinthaisuru444@gmail.com>
2019-09-11 22:31:37 +0530

    Added the collect order date to the system

--- app/src/main/java/com/ayesha/shoppingcart/CartFragment.java
+++ app/src/main/java/com/ayesha/shoppingcart/CartFragment.java
@@ -1,6 +1,5 @@
 package com.ayesha.shoppingcart;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -14,8 +13,6 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityOptionsCompat;
-import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -124,7 +121,7 @@ public class CartFragment extends Fragment {
                         .setPositiveButton("Collect Delivery", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 Intent intent = new Intent(CartFragment.this.getContext(), CollectDeliveryActivity.class);
-                                //intent.putExtra("price", CartFragment.this.price.getText());
+                                intent.putExtra("price", CartFragment.this.price.getText());
                                 context.startActivity(intent);
                             }
                         })
--- app/src/main/java/com/ayesha/shoppingcart/CollectDeliveryActivity.java
+++ app/src/main/java/com/ayesha/shoppingcart/CollectDeliveryActivity.java
@@ -3,12 +3,56 @@ package com.ayesha.shoppingcart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class CollectDeliveryActivity extends AppCompatActivity {
+
+    private TextView price;
+    private TextView collectDate;
+    private MaterialButton confirm;
+    private MaterialButton close;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.collect_delivery);
+
+        price = findViewById(R.id.price);
+        price.setText(getIntent().getStringExtra("price"));
+
+        collectDate = findViewById(R.id.collectDate);
+
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, +7);
+        SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:00:00");
+
+        collectDate.setText(""+format1.format(cal.getTime()));
+
     }
+
+    private void setConfirmClickListner(){
+        this.confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void setCloseClickListner(){
+        this.close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+    }
+
+
 }
